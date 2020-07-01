390c1e9a43ef987bfd50fc3d1b5176b0c36f45f4
Gerardo Flores <gerardo.gabriel.flores@gmail.com>
2020-06-30 23:05:01 -0500

    ya funciona lo de cambio de dolares a soles y de soles a dolares

--- app/src/main/java/com/gerardo/desafiobcp/view/ui/activity/MainActivity.kt
+++ app/src/main/java/com/gerardo/desafiobcp/view/ui/activity/MainActivity.kt
@@ -3,6 +3,7 @@ package com.gerardo.desafiobcp.view.ui.activity
 import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.gerardo.desafiobcp.R
 import com.gerardo.desafiobcp.view.ui.utils.ChangeMoney
@@ -11,7 +12,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var operation: ChangeMoney
+    lateinit var operation: ChangeMoney
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -21,13 +22,33 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         changeMoneyValue()
-        juegoDeComprayVenta()
+        calculoDeArriba()
+        calculoDeAbajo()
     }
 
     private fun changeMoneyValue() {
 
-        moneyChange.setOnClickListener {
 
+        //TODO NO FUNCIONA ESTA MIERDA! Y ME DESESPERA !!!
+        /*
+        txtMoneyOut.addTextChangedListener(object : SimpleTextWatcher() {
+            override fun afterTextChanged(s: Editable?) {
+                val nuevoValor = (txtMoneyIn.text.toString().trim().toDouble()) * 3.29
+                Log.wtf("VENTA", "VALOR ANTIGUO$nuevoValor")
+                txtMoneyIn.setText(nuevoValor.toString())
+            }
+        })
+
+        txtMoneyIn.addTextChangedListener(object : SimpleTextWatcher() {
+            override fun afterTextChanged(s: Editable?) {
+                val nuevoValor = (txtMoneyIn.text.toString().trim().toDouble()) * 3.25
+                Log.wtf("COMPRA", "VALOR ANTIGUO$nuevoValor")
+                txtMoneyOut.setText(nuevoValor.toString())
+
+            }
+        })*/
+
+        moneyChange.setOnClickListener {
             if (btnChangeIcon.text == DOLARES) {
                 Thread.sleep(100)
                 btnChangeIcon.text = "Soles"
@@ -43,26 +64,29 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun juegoDeComprayVenta() {
-
-        if (btnChangeIcon.text == DOLARES) {
-            txtMoneyOut.addTextChangedListener(object : SimpleTextWatcher() {
-                override fun afterTextChanged(s: Editable?) {
-                    operationCompraDolares()
-                }
-            })
-        } else {
-            txtMoneyIn.addTextChangedListener(object : SimpleTextWatcher() {
-                override fun afterTextChanged(s: Editable?) {
-                    operationVentaDolares()
-                }
-            })
+    private fun calculoDeArriba() {
+        btnOperationChange.setOnClickListener {
+            if (btnChangeIcon.text == DOLARES && btnChangeIconOut.text == SOLES) {
+                val nuevoValor =
+                    (txtMoneyOut.text.toString().trim().toDouble()) / TIPO_CAMBIO_COMPRA
+                txtMoneyIn.setText(nuevoValor.toString())
+            } else {
+                val nuevoValor =
+                    (txtMoneyOut.text.toString().trim().toDouble()) * TIPO_CAMBIO_VENTA
+                txtMoneyIn.setText(nuevoValor.toString())
+            }
         }
     }
 
-    private fun finalOperation() {
-        btnOperationChange.setOnClickListener {
-
+    private fun calculoDeAbajo() {
+        btnOperationChange2.setOnClickListener {
+            if (btnChangeIcon.text == DOLARES && btnChangeIconOut.text == SOLES) {
+                val nuevoValor = (txtMoneyIn.text.toString().trim().toDouble()) * TIPO_CAMBIO_COMPRA
+                txtMoneyOut.setText(nuevoValor.toString())
+            } else {
+                val nuevoValor = (txtMoneyIn.text.toString().trim().toDouble()) / TIPO_CAMBIO_VENTA
+                txtMoneyOut.setText(nuevoValor.toString())
+            }
         }
     }
 
@@ -71,27 +95,29 @@ class MainActivity : AppCompatActivity() {
         startActivity(nextActivity)
     }
 
-
     fun operationCompraDolares() {
-        val nuevoValor = operation.solesToDolares(
-            (txtMoneyOut.text.toString().trim().toDouble())
-        )
-        txtMoneyOut.setText(nuevoValor.toString())
+        val nuevoValor = (txtMoneyIn.text.toString().trim().toDouble()) / 3.25
+        Log.wtf("COMPRA", "VALOR ANTIGUO$nuevoValor")
+        txtMoneyIn.setText(nuevoValor.toString())
     }
 
     fun operationVentaDolares() {
-        val nuevoValor = operation.dolaresToSoles(
-            (txtMoneyIn.text.toString().trim().toDouble())
-        )
-        txtMoneyIn.setText(nuevoValor.toString())
+        val nuevoValorDolares = (txtMoneyIn.text.toString().trim().toDouble()) * 3.29
+        Log.wtf("VENTA", "VALOR ANTIGUO$nuevoValorDolares")
+        txtMoneyOut.setText(nuevoValorDolares.toString())
     }
 
 
     companion object {
+
+        private const val TIPO_CAMBIO_VENTA = 3.29
+        private const val TIPO_CAMBIO_COMPRA = 3.25
+
         private const val SOLES = "Soles"
         private const val DOLARES = "Dolares"
         private const val EURO = "E"
         private const val YEN = "Y"
+
 
     }
 }
--- app/src/main/java/com/gerardo/desafiobcp/view/ui/utils/ChangeMoney.kt
+++ app/src/main/java/com/gerardo/desafiobcp/view/ui/utils/ChangeMoney.kt
@@ -1,21 +1,24 @@
 package com.gerardo.desafiobcp.view.ui.utils
 
+
 class ChangeMoney {
 
-    // TODO SI VOY CON SOLES ES QUE VOY A COMPRANDO
-    fun solesToDolares(cantidad: Double): Double {
-        return cantidad / DOLARES_COMPRA
+    // todo  >--- EL TIPO DE CAMBIO DE COMPRA ES MENOR QUE EL DE VENTA <---
+
+    // TODO SI VOY CON DOLARES ES QUE VOY A VENDER <------ VENTA
+    fun dolaresToSoles(cantidad: Double): Double {
+        return cantidad * DOLARES_VENTA
     }
 
-    // TODO SI VOY CON DOLARES ES QUE VOY A VENDER
-    fun dolaresToSoles(cantidad: Double): Double {
-        return cantidad * SOLES_VENTA
+    // TODO SI VOY CON SOLES ES QUE VOY A COMPRANDO <----- COMPRAAA!
+    fun solesToDolares(cantidad: Double): Double {
+        return cantidad / DOLARES_COMPRA
     }
 
 
     companion object {
         private const val DOLARES_COMPRA = 3.25
-        private const val SOLES_VENTA = 3.29
+        private const val DOLARES_VENTA = 3.29
         private const val EUROPEAN_UNION = 4.50
         private const val JAPAN = 5.00
     }
