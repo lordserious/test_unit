26f349a2f23c20dd27fdcae00caeb864235ab844
Gerardo Flores <gerardo.gabriel.flores@gmail.com>
2020-07-01 12:03:30 -0500

    ChangeMoney

--- app/src/main/java/com/gerardo/desafiobcp/view/ui/utils/ChangeMoney.kt
+++ app/src/main/java/com/gerardo/desafiobcp/view/ui/utils/ChangeMoney.kt
@@ -5,16 +5,53 @@ class ChangeMoney {
 
     // todo  >--- EL TIPO DE CAMBIO DE COMPRA ES MENOR QUE EL DE VENTA <---
 
-    // TODO SI VOY CON DOLARES ES QUE VOY A VENDER <------ VENTA
     fun dolaresToSoles(cantidad: Double): Double {
         return cantidad * TIPO_CAMBIO_VENTA_DOLAR
     }
 
-    // TODO SI VOY CON SOLES ES QUE VOY A COMPRANDO <----- COMPRAAA!
     fun solesToDolares(cantidad: Double): Double {
         return cantidad / TIPO_CAMBIO_COMPRA_DOLAR
     }
 
+    fun euroToSoles(cantidad: Double): Double {
+        return cantidad * TIPO_CAMBIO_VENTA_EURO
+    }
+
+    fun solesToEuro(cantidad: Double): Double {
+        return cantidad / TIPO_CAMBIO_COMPRA_EURO
+    }
+
+    fun librasToSoles(cantidad: Double): Double {
+        return cantidad * TIPO_CAMBIO_VENTA_LIBRA
+    }
+
+    fun solesToLibras(cantidad: Double): Double {
+        return cantidad / TIPO_CAMBIO_COMPRA_LIBRA
+    }
+
+    fun yenToSoles(cantidad: Double): Double {
+        return cantidad * TIPO_CAMBIO_VENTA_YEN
+    }
+
+    fun solesToYen(cantidad: Double): Double {
+        return cantidad / TIPO_CAMBIO_COMPRA_YEN
+    }
+
+    fun dolarCanadienseToSoles(cantidad: Double): Double {
+        return cantidad * TIPO_CAMBIO_VENTA_DOLAR_CANADIENSE
+    }
+
+    fun solesToDolarCanadiense(cantidad: Double): Double {
+        return cantidad / TIPO_CAMBIO_COMPRA_DOLAR_CANADIENSE
+    }
+
+    fun francoSuizoToSoles(cantidad: Double): Double {
+        return cantidad * TIPO_CAMBIO_VENTA_FRANCO_SUIZO
+    }
+
+    fun solesToFrancoSuizo(cantidad: Double) : Double{
+        return cantidad / TIPO_CAMBIO_COMPRA_FRANCO_SUIZO
+    }
 
     companion object {
 
@@ -23,24 +60,24 @@ class ChangeMoney {
         private const val TIPO_CAMBIO_COMPRA_DOLAR = 3.25
 
         //Euros
-        private const val TIPO_CAMBIO_VENTA_EURO = 4.10
-        private const val TIPO_CAMBIO_COMPRA_EURO = 4.20
+        private const val TIPO_CAMBIO_VENTA_EURO = 4.20
+        private const val TIPO_CAMBIO_COMPRA_EURO = 4.10
 
         //Libras
-        private const val TIPO_CAMBIO_VENTA_LIBRA = 5.00
-        private const val TIPO_CAMBIO_COMPRA_LIBRA = 5.50
+        private const val TIPO_CAMBIO_VENTA_LIBRA = 5.50
+        private const val TIPO_CAMBIO_COMPRA_LIBRA = 5.10
 
         //Yen
-        private const val TIPO_CAMBIO_VENTA_YEN = 5.00
-        private const val TIPO_CAMBIO_COMPRA_YEN = 5.50
+        private const val TIPO_CAMBIO_VENTA_YEN = 5.50
+        private const val TIPO_CAMBIO_COMPRA_YEN = 5.10
 
         //Dolar Canadiense
-        private const val TIPO_CAMBIO_VENTA_DOLAR_CANADIENSE = 2.29
-        private const val TIPO_CAMBIO_COMPRA_DOLAR_CANADIENSE = 2.50
+        private const val TIPO_CAMBIO_VENTA_DOLAR_CANADIENSE = 2.59
+        private const val TIPO_CAMBIO_COMPRA_DOLAR_CANADIENSE = 2.20
 
         //Franco Suizo
-        private const val TIPO_CAMBIO_VENTA_FRANCO_SUIZO = 2.29
-        private const val TIPO_CAMBIO_COMPRA_FRANCO_SUIZO = 2.50
+        private const val TIPO_CAMBIO_VENTA_FRANCO_SUIZO = 2.50
+        private const val TIPO_CAMBIO_COMPRA_FRANCO_SUIZO = 2.29
 
         private const val SOLES = "Soles"
         private const val DOLARES = "Dolares"
